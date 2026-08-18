d21debb07e9c18e3710e5256de64bf6dc23d5923
Pranaw Rai <dhanrajrai2351@gmail.com>
2026-08-18 15:06:41 +0545

    Added password hashing and token generation logic

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import jsonwebtoken from 'jsonwebtoken'
 
 const userSchema = new mongoose.Schema({
     fullName: {
@@ -28,3 +30,40 @@ const userSchema = new mongoose.Schema({
 }, {timestamps: true})
 
 export const User = mongoose.model("User", userSchema)
+
+
+userSchema.pre("save", async function(password) {
+    if(!this.isModified("password")) return
+
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccesToken = function() {
+    return jwt.sign(
+        {
+            _id: this.id,
+            email: this.email,
+            "username": this,username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = function() {
+    return jwt.sign(
+        {
+            _id: this.id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
